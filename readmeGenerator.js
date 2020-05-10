8be91118d239c7f79492a8798dd9515d96975392
Michael P Nance <michael.p.nance@gmail.com>
2020-05-09 21:44:50 -0400

    pushing new code through GitHub UI
    Since I added the licence and modified the readme through github's editor it's complaining about merge conflict when I try to push my local code.

--- readmeGenerator.js
+++ readmeGenerator.js
@@ -1,8 +1,10 @@
 "use strict";
 
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 (async() => {
+
     const input = await inquirer.prompt([{
         message: "Title",
         name: "title",
@@ -10,109 +12,55 @@ const inquirer = require("inquirer");
         message: "Description",
         name: "description",
     }, {
+        message: "Usage",
+        name: "usage",
+    }, {
         message: "Installation",
         name: "installation",
     }, {
-        message: "Usage",
-        name: "usage",
+        message: "Tests",
+        name: "tests",
+    }, {
+        message: "Authors",
+        name: "authors",
+    }, {
+        message: "Contributing",
+        name: "contributing",
     }, {
         message: "License",
         name: "license",
     }, {
-        message: "Contributors",
-        name: "contributors",
+        message: "GitHub account",
+        name: "account",
     }, {
-        message: "Tests",
-        name: "tests",
+        message: "Repository",
+        name: "repo",
     }]);
-    console.log(input);
+
+    // Returns table of contents corresponding to user input
+    function genTOC(input) {
+        let toc = '';
+        if (input.usage != '') toc += "- [Usage](#Usage)\n";
+        if (input.installation != '') toc += "- [Installing](#Installing)\n";
+        if (input.tests != '') toc += "- [Running the Tests](#Running-the-Tests)\n";
+        if (input.authors != '') toc += "- [Authors](#Authors)\n";
+        if (input.contributing != '') toc += "- [Contributing](#Contributing)\n";
+        if (input.license != '') toc += "- [License](#License)\n";
+        return toc;
+    }
+
+    // Returns readme text corresponding to user input
+    function genREADME(input) {
+        let readme = `# ${input.title}\n\n${input.description}\n\n## Table of Contents\n\n${genTOC(input)}`;
+        if (input.account != '' && input.repo != '') readme = `![GitHub commit activity](https://img.shields.io/github/commit-activity/m/${input.account}/${input.repo})\n\n` + readme;
+        if (input.usage != '') readme += `## Usage\n\n${input.usage}\n\n`
+        if (input.installation != '') readme += `## Installing\n\n${input.installation}\n\n`
+        if (input.tests != '') readme += ` ## Running the tests\n\n${input.tests}\n\n`
+        if (input.authors != '') readme += ` ## Authors\n\n${input.authors}\n\n`
+        if (input.contributing != '') readme += ` ## Contributing\n\n${input.contributing}\n\n`
+        if (input.license != '') readme += ` ## License\n\n${input.license}\n\n`
+        return readme;
+    }
+
+    fs.writeFile("README.md", genREADME(input), "utf8", err => { if (err) console.log(err) });
 })();
-
-const template = `# Project Title
-
-One Paragraph of project description goes here
-
-## Getting Started
-
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
-
-### Prerequisites
-
-What things you need to install the software and how to install them
-
-\`\`\`
-Give examples
-\`\`\`
-
-### Installing
-
-A step by step series of examples that tell you how to get a development env running
-
-Say what the step will be
-
-\`\`\`
-Give the example
-\`\`\`
-
-And repeat
-
-\`\`\`
-until finished
-\`\`\`
-
-End with an example of getting some data out of the system or using it for a little demo
-
-## Running the tests
-
-Explain how to run the automated tests for this system
-
-### Break down into end to end tests
-
-Explain what these tests test and why
-
-\`\`\`
-Give an example
-\`\`\`
-
-### And coding style tests
-
-Explain what these tests test and why
-
-\`\`\`
-Give an example
-\`\`\`
-
-## Deployment
-
-Add additional notes about how to deploy this on a live system
-
-## Built With
-
-* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
-* [Maven](https://maven.apache.org/) - Dependency Management
-* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
-
-## Contributing
-
-Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
-
-## Versioning
-
-We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
-
-## Authors
-
-* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
-
-See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
-
-## License
-
-This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
-
-## Acknowledgments
-
-* Hat tip to anyone whose code was used
-* Inspiration
-* etc
-`
